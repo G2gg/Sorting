1e34fa1500abed107846edbe280c2224fc8330d3
om <om@DESKTOP-G70MTG1.mshome.net>
2022-09-06 16:53:52 +0530

    New way of sorting.

---
+++ src/NewSort.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class NewSort {
+	
+	public static void selectionSort(int[] arr){  
+        for (int i = 0; i < arr.length - 1; i++)  
+        {  
+            int index = i;  
+            for (int j = i + 1; j < arr.length; j++){  
+                if (arr[j] < arr[index]){  
+                    index = j;//searching for lowest index  
+                }  
+            }  
+            int smallerNumber = arr[index];   
+            arr[index] = arr[i];  
+            arr[i] = smallerNumber;  
+        }  
+    }  
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		
+		int[] arr1 = new int[n]; 
+		
+		for (int i = 0; i < n; i++) {
+			arr1[i] = s.nextInt();
+		}
+		
+        System.out.println("Before Selection Sort");  
+        for(int i:arr1){  
+            System.out.print(i+" ");  
+        }  
+        System.out.println();  
+          
+        selectionSort(arr1);//sorting array using selection sort  
+         
+        System.out.println("After Selection Sort");  
+        for(int i:arr1){  
+            System.out.print(i+" ");  
+        }  
+    }  
+	
+}
